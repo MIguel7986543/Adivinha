0ba6df7e68599fde0875f2d4ca5da8b94153add3
miguel <mdias1300@gmail.com>
2025-03-07 22:40:59 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Thread.Sleep(3000);
+
+Console.WriteLine(@"o que é, o que é...");
+
+Thread.Sleep(3000);
+
+Console.WriteLine("o que o peixe faz para atravessar a rua?");
+
+Thread.Sleep(5000);
+
+Console.WriteLine("nada.");
